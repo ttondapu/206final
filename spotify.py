2520912a141265a1c1ead5556ee53e09ffddc6df
ttondapu <ttondapu@umich.edu>
2022-04-19 14:08:41 -0400

    4/19 2:08

--- spotify.py
+++ spotify.py
@@ -9,6 +9,12 @@ import unittest
 import csv
 import matplotlib.pyplot as plt
 
+def createDB(filename):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+filename)
+    cur = conn.cursor()
+    return cur, conn
+
 def makeurl(type, id):
     #spotify:artist:4O15NlyKLIASxsJ0PrXPfz
     #spotify:track:4cOdK2wGLETKBW3PvgPWqT
@@ -19,24 +25,14 @@ def makeurl(type, id):
 def getalbum(id, offset): #update token every time :/
     token = 'BQDXWd9VOfS3z_ItPUv4knplzIZpfT_b3y1x7Uy6VSh7FcZ0_vRukhcWlAR9y_O8_sOqRntSHtJBIO-sXz_OdDsI9cK3j0hktveV5qhNHC7nVLzN2xMhDY4dmYV0cZOtipnUUMA-BucOJ4MPIBM9dU09cSIsMLb0znU'
     url = makeurl("albums", id)
-    print()
-    print(url)
-    print()
     param = {'limit': 25,'offset': offset, 'access_token': token}
     response = requests.get(url, params = param)
     data = json.loads(response.text)
     print(data)
 
-def createDB(filename):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+filename)
-    cur = conn.cursor()
-    return cur, conn
-
 def main():
-    cur, conn = createDB('JRP.db')
-
-    getalbum("4O15NlyKLIASxsJ0PrXPfz", start)
+    cur, conn = createDB('spotify.db')
+    getalbum("4O15NlyKLIASxsJ0PrXPfz", "WHAT GOES HERE?")
 
 main()
 
