a190552cc86d704d90537171e9d521a3c5ee0388
ttondapu <ttondapu@umich.edu>
2022-04-20 15:02:33 -0400

    pulling data, next step is to add to database

--- spotify.py
+++ spotify.py
@@ -15,26 +15,58 @@ def createDB(filename):
     cur = conn.cursor()
     return cur, conn
 
-def makeurl(type, id):
+def artistalbumsurl(id):
     #spotify:artist:4O15NlyKLIASxsJ0PrXPfz
     #spotify:track:4cOdK2wGLETKBW3PvgPWqT
-    baseurl = 'https://api.spotify.com/v1/'
-    baseurl = baseurl + type + "/" + id
-    return baseurl
+    return "https://api.spotify.com/v1/artists/" + id + "/albums"
 
-def getalbum(id, offset): #update token every time :/
-    token = 'BQDXWd9VOfS3z_ItPUv4knplzIZpfT_b3y1x7Uy6VSh7FcZ0_vRukhcWlAR9y_O8_sOqRntSHtJBIO-sXz_OdDsI9cK3j0hktveV5qhNHC7nVLzN2xMhDY4dmYV0cZOtipnUUMA-BucOJ4MPIBM9dU09cSIsMLb0znU'
-    url = makeurl("albums", id)
+def albumurl(id):
+    return "https://api.spotify.com/v1/albums/" + id
+
+def get_artist_albums(artistid, offset, cur): 
+    #must update token every time :/
+    #link here https://developer.spotify.com/console/get-album/
+    token = 'BQBbCpsLEy0Z1jAwxsJkSIWytwQvO4mHQMp8L5U9P_V01Wea6jZAfmUREVzQ1QvMjHTcX7nyFrt9AwK2IV0m2TMhw8KT98pKc2Mkl9YUDspXXcPAcXROD447f6fisnMOdPvR5ckdSXDuQsmw6KrMxOc1V50xu1ejGdw'
+    url = artistalbumsurl(artistid)
     param = {'limit': 25,'offset': offset, 'access_token': token}
     response = requests.get(url, params = param)
-    data = json.loads(response.text)
-    print(data)
+    results = response.json()
+    albumids = []
+    for x in results['items']:
+        albumids.append(x['id'])
+    data = []
+    for y in albumids:
+        url = albumurl(y)
+        param = {'access_token' : token}
+        response = requests.get(url, params = param)
+        results = response.json()
+        title = results['name']
+        date = results['release_date']
+        totaltracks = int(results['total_tracks'])
+        #feel free to add whatever data u want to gather here
+        #https://developer.spotify.com/documentation/web-api/reference/#/operations/get-an-album
+        #list of possible keys ^
+        if totaltracks > 3: #to ensure we aren't including singles as albums
+            data.append((title, date, totaltracks))
+    return data
 
 def main():
+    favartists = {'Lil Uzi Vert' : '4O15NlyKLIASxsJ0PrXPfz',
+        'Olivia Rodrigo' : '1McMsnEElThX1knmY4oliG', 
+        'Ed Sheeran' : "6eUKZXaKkcviH0Ku9w2n3V",
+        'Post Malone' : '246dkjvS1zLTtiykXe5h60',
+        'The Weeknd' : '1Xyo4u8uXC1ZmMpatF05PJ',
+        'Ariana Grande' : '66CXWjxzNUsdJxJ2JdwvnR',
+        'Justin Bieber' : '1uNFoZAHBGtllmzznpCI3s'}
+
     cur, conn = createDB('spotify.db')
-    getalbum("4O15NlyKLIASxsJ0PrXPfz", "WHAT GOES HERE?")
+    try:
+        cur.execute('SELECT name FROM spotify_results WHERE name  = Lil Uzi Vert')
+        start = cur.fetchone()
+        start = start[0]
+    except:
+        start = 0
+    data = get_artist_albums(favartists['Olivia Rodrigo'], start, cur)
+    print(data) #to see if the api is working
 
 main()
-
-
-
