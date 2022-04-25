51329289f1ea8f481c69d058ca5d1a0ab094b476
Ambika Tripathi <ambikat@0587299691.wireless.umich.net>
2022-04-24 20:17:12 -0400

    finished num_tracks

--- soundcloud.py
+++ soundcloud.py
@@ -16,10 +16,9 @@ def createDB(filename):
     cur = conn.cursor()
     return cur, conn
 
-def get_html(artist_user):
-    url = 'https://soundcloud.com/'+ artist_user + '/albums'
-    page = requests.get(url)
-    return page.text
+# def get_url(artist_user):
+#     url = 'https://soundcloud.com/'+ artist_user + '/albums'
+#     return url
 
 def artist_followers(artist_html):
 
@@ -27,10 +26,10 @@ def artist_followers(artist_html):
     generates the number of followers for a given artist and file saved from an artist's SoundCloud page
 
     '''
-    # with open(artist_html) as f:
-    #     soup = bs(f, 'html.parser')
-    soup = bs(artist_html, 'html.parser')
-    soup.prettify()   
+    with open(artist_html) as f:
+        soup = bs(f, 'html.parser')
+    # page = requests.get(url)
+    # soup = bs(page.text, 'html.parser')
     num_followers = soup.find('a', class_ = "infoStats__statLink sc-link-light sc-link-primary").get('title')
     s = num_followers.split()
     s = s[0].replace(',', '') #to get rid of commas
@@ -38,25 +37,27 @@ def artist_followers(artist_html):
     return num
 
 def album_count(artist_html):
-    num_albums = 0
-    # with open(artist_html) as f:
-    #     soup = bs(f, 'html.parser')
-    soup = bs(artist_html, 'html.parser')
-    soup.prettify()
-    album_list = []
-    album_tag = soup.find_all('a', class_ = "sc-link-primary soundTitle__title sc-link-dark sc-text-h4")
-    for tag in album_tag:
-        title = tag.find('span', class_="").text.strip()
-        album_list.append(title)
-    for album in album_list:
-        num_albums += 1
-    return num_albums
+    num_tracks = 0
+    with open(artist_html) as f:
+        soup = bs(f, 'html.parser')
+    # page = requests.get(url)
+    # soup = bs(page.text, 'html.parser') #requests
+    track_list = []
+    track_tag = soup.find_all('a', class_ = "sc-link-primary soundTitle__title sc-link-dark sc-text-h4")
+    for tag in track_tag:
+        title = tag.find('span', class_ = '').text.strip()
+        track_list.append(title)
+    for track in track_list:
+        num_tracks += 1
+  
+    return num_tracks
+
 
 
 #line 1694
-artists = {
+artists = { #change
     'Ed Sheeran': ['edsheeran'], 
-    'Olivia Rodrigo': ['oliviarodrigo'], 
+    'Olivia Rodrigo': ['oliviarodrigo', 'olivia_rodrigo.html'], 
     'The Weeknd': ['theweeknd'],
     'Ariana Grande': ['arianagrande'],
     'Taylor Swift': ['taylorswiftofficial'],
@@ -67,7 +68,3 @@ artists = {
     'Juice Wrld': ['uiceheidd']
 }
 
-pretty(get_html(artists['Olivia Rodrigo'][0]))
-
-# def main():
-#     print('hi')
